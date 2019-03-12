ba3e14b8dfe4f4762c18deff7522d21b4b4ee564
Michele Saba <michelemsaba@gmail.com>
2019-03-12 06:59:44 -0500

    created second page and app.js file

---
+++ js/app.js
@@ -0,0 +1,32 @@
+/*var today = new Date();
+document.write (today);
+var name = "Michele";
+var count = 5; 
+var plusOne = 2; 
+var hello = "hello"
+console.log(count + plusOne);
+
+
+console.log('hello world', count, name);
+var programmer = "michele";
+var hello = "hello";
+var its = 1;
+
+for (var i = 0; i < its; i++) {
+    //alert(hello);
+    console.log(hello);
+    document.write(hello);
+    prompt("how old are you?")
+}
+*/
+var noun = prompt("Give me a noun");
+var verb = prompt("Give me a verb");
+var adjective1 = prompt("Give me an adjective");
+var name = prompt("name");
+
+document.write(name + ' ' + 'likes to' + ' ' + verb + ' ' + 'while smelling' + ' ' + ' ' + adjective1 + ' ' +  noun);
+
+console.log(noun, "The users name");
+console.log(verb, "The users verb");
+console.log(adjective1, "The users adjective1");
+console.log(name, "The users name");
